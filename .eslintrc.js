9cae8eefcf7d75652419325e05d65e71f7b89968
Granger <923481530@qq.com>
2022-10-13 19:49:52 +0800

    1

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,17 +1,56 @@
+// module.exports = {
+//   root: true,
+//   env: {
+//     node: true
+//   },
+//   extends: [
+//     'plugin:vue/essential',
+//     '@vue/standard'
+//   ],
+//   parserOptions: {
+//     parser: 'babel-eslint'
+//   },
+//   rules: {
+//     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+//     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
+//   }
+// }
+
+
 module.exports = {
+  // 代表eslitn是否是根
   root: true,
+  // 环境变量
   env: {
-    node: true
+    node: true, // 开启node的环境变量,
+    browser: true // 是否开启浏览器的环境变量
   },
-  extends: [
-    'plugin:vue/essential',
-    '@vue/standard'
-  ],
+  // 继承
+  // airbnb
+  // vue
+  // plugin:vue ---> eslint-plugin-vue configs的 essential
+  // @vue/eslint-config-standard
+  extends: ['plugin:vue/essential', '@vue/standard'],
   parserOptions: {
+    // eslit的解析器, 也是有一个包
+    // parser: '@babel/eslint-parser'
     parser: 'babel-eslint'
   },
+  // 自定义规则
+  //  新增或者关闭一些规则则
+  // off/0是关闭, error,2错误, warn,1 警告
   rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
+    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'space-before-function-paren': 0
   }
 }
+
+//
+// 关闭规则的方式一: eslintrc里使用rules
+// 关闭规则的方式二:
+//  - /* eslint-disable */ 从此条注释往下, 禁用eslint规则
+//  -  /* eslint-disable-next-line */  禁用 下一行eslint规则
+//  - /* eslint-disable-line */ 禁用 本行eslint规则
+// 关闭规则三
+// eslintigonre  创建这个文件，可以让所在文件不需要进行eslint检查
